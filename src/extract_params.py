de7727c51267ca177eed140e7a8cc475dd26451e
Misha Turnbull <mishaturnbull@gmail.com>
2021-02-13 19:28:01 -0600

    Add much more useful parameter extraction
    Supports differentiating between first and last setting of parameters, warning about duplicates
    
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/extract_params.py
+++ src/extract_params.py
@@ -6,10 +6,33 @@ Extract parameters from a log file and write them to a specified .param file.
 """
 
 import os
+import re
+import sys
 
-def get_text_to_write(log):
+from . import message_remover
+
+MULTIVAL_FIRST = 0x01
+MULTIVAL_LAST = 0x02
+MULTIVAL_FAIL = 0x04
+MULTIVAL_WARN = 0x08
+
+
+def params_to_filecontents(params):
+    """
+    Given a dictionary of parameters, mangle it into a string that can be
+    written to a file.
+
+    (Don't use .writelines() on this -- file.write(...)!)
+    """
+    lines = []
+    for key, val in params.items():
+        lines.append("{},{}".format(key, str(val)))
+    return '\n'.join(lines)
+
+def grab_params_simple(log):
     """
     Given a log file, return the file contents of the output .param file.
+    Always returns the last occuring value of a parameter.
     """
     # sometimes the dfreader fails to completely parse the file for some reason
     # ensure it's done before continuing
@@ -17,11 +40,86 @@ def get_text_to_write(log):
         m = log.recv_msg()
         if m is None:
             break
-    params = log.params
-    lines = []
-    for key, val in params.items():
-        lines.append("{},{}".format(key, str(val)))
-    return '\n'.join(lines)
+    return log.params
+
+def grab_params_complex(log, multivalhandle=None, paramfilter=None):
+    """
+    Grab parameters in a more advanced manner allowing for better handling of
+    non-unique parameter responses and filtering.
+
+    Multivalhandle may be set to None (default), MULTIVAL_FIRST, MULTIVAL_LAST,
+    or MULTIVAL_FAIL.  _FIRST will record the first instance of the parameter
+    being set; _last will record the last instance; and _FAIL will cause a
+    ValueError to be raised on a duplicate parameter.  Any of those options
+    may also be combined (via addition) with MULTIVAL_WARN, which
+    will cause a printed warning to be emmitted to stdout on each duplicate
+    value.  Default behavior is MULTIVAL_LAST.
+
+    Filtering is accomplished with a list of strings.  Each string is compiled
+    to a regex object, A parameter name matching any of the objects will be
+    recorded; non-matching parameter names are ommitted from the output.
+    Default behavior matches everything.
+    """
+
+    if paramfilter is None:
+        paramfilter = ['.*']
+    paramfilter = [re.compile(f) for f in paramfilter]
+
+    # check for illegal parameters
+    sets = [MULTIVAL_FIRST & multivalhandle,
+            MULTIVAL_LAST & multivalhandle,
+            MULTIVAL_FAIL & multivalhandle]
+    n = 0
+    for i in range(len(sets)):
+        if sets[i]:
+            n += 1
+    if n > 1:
+        raise AttributeError("Invalid arguments supplied!")
+    if n == 0:
+        multivalhandle = MULTIVAL_LAST
+
+    # process!  we're actually going to use the message filter here to grab
+    # all the PARM messages, since those contain the parameters.  The rest
+    # will be discarded
+    parm, _ = message_remover.filter_packet_type(log, ['PARM'])
+    del _
+
+    params = {}
+    for msg in parm:
+        key, val = msg.Name, msg.Value
+        # first, does it match the filter?
+        matching = False
+        for reo in paramfilter:
+            if reo.match(key):
+                matching = True
+                break
+        if not matching:
+            continue
+
+        # now, check for duplicates
+        if key in params.keys():
+            # uh-oh!  duplicate!
+            if multivalhandle & MULTIVAL_WARN:
+                # let the user know
+                print("Duplicate parameter: {}".format(msg.to_dict()))
+            if multivalhandle & MULTIVAL_FIRST:
+                # if only want the first parameter, skip. messages are sorted
+                # by arrival time, so anything after the first one comes later
+                continue
+            elif multivalhandle & MULTIVAL_LAST:
+                # if only want the last value, overwrite it with this occurance
+                params[key] = val
+            elif multivalhandle & MULTIVAL_FAIL:
+                # commit honorable seppuku
+                raise ValueError("Duplicate parameters detected!")
+            else:
+                # no behavior specified... hrmph.
+                pass
+        else:
+            # no duplication, good to continue processing normally
+            params.update({key: val})
+    return params
+    
 
 def write_out_file(text, filename, force=False):
     """
@@ -40,8 +138,18 @@ if __name__ == '__main__':
     infilename = sys.argv[1]
     outfilename = '.'.join([os.path.splitext(infilename)[0], 'param'])
     
-    log = dfr.DFReader_binary(infilename)
-    write_out_file(get_text_to_write(log), outfilename)
+    if os.path.splitext(infilename)[1].lower() == '.bin':
+        cls = dfr.DFReader_binary
+    elif os.path.splitext(infilename)[1].lower() == '.log':
+        cls = dfr.DFReader_text
+    else:
+        print("I don't know how to open that!")
+        sys.exit(1)
+
+    log = cls(infilename)
+    params = grab_params_simple(log)
+    lines = params_to_filecontents(params)
+    write_out_file(lines, outfilename)
 
     print("Done!")
     
