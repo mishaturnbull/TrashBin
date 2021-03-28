2aff4faf60fe83be88a12991437137645d00d259
Misha Turnbull <mishaturnbull@gmail.com>
2021-03-28 14:36:33 -0500

    Plugin loading box complete (although untested)
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/gui/mainwindow.py
+++ src/gui/mainwindow.py
@@ -20,29 +20,63 @@ class PluginLoaderPanel(object):
     Creates the plugin load selector user interface.
     """
 
-    def __init__(self, root, plugins_available):
-        self.parent = root
+    def __init__(self, parent, plugins_available):
+        self.parent = parent
         self.plugins_available = plugins_available
         self.selected = []
         self.spawn_ui()
+        self.root.mainloop()
+
+    def cb_loadsel(self):
+        selected = self.pluginselbox.curselected()
+        for i in selected:
+            plugin = self.plugins_available[i]
+            self.parent.plugins.append(plugin)
+
+    def cb_describe(self):
+        selected = self.pluginselbox.curselected()
+        plugin = self.plugins_available[selected[0]]
+        text = "Author: {}\n{}\n\n{}".format(
+                plugin.author_name, plugin.author_email, plugin.plugin_desc)
+        # now that we have the text, first delete anything currently in the
+        # description box to clear it out
+        self.detailsbox.delete(0, tk.END)
+        # now put our text in!
+        self.detailsbox.insert(0, text)
 
     def spawn_ui(self):
         """
         Spawns UI components.
         """
-        self.root = tk.Toplevel(self.parent)
+        self.root = tk.Toplevel(self.parent.root)
         self.root.resizable(False, False)
 
         # plugin listbox and scrollbar
         listframe = tk.Frame(self.root)
         listframe.grid(row=0, column=0)
-        self.pluginselbox = tk.Listbox(listframe, height=20, width=40,
-                selectmode="multiple")
+        self.pluginselbox = tk.Listbox(listframe, height=20, width=40)
         self.pluginselbox.pack(side=tk.LEFT, fill=tk.BOTH)
         scrollbar = tk.Scrollbar(listframe)
-        scrollbar.grid(side=tk.RIGHT, fill=tk.BOTH)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
         self.pluginselbox.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.pluginselbox.yview)
+
+        addbtn = tk.Button(self.root, text="Load plugin",
+                command=self.cb_loadsel)
+        addbtn.grid(row=1, column=0, columnspan=2, sticky='nesw')
+        self.pluginselbox.bind("<<ListboxSelect>>", self.cb_describe)
+
+        # populate the plugin list
+        for plugin in self.plugins_available:
+            self.pluginselbox.insert(tk.END. plugin.plugin_name)
+
+        # plugin details box
+        listframe.update()
+        rightframe = tk.Frame(self.root, width=listframe.winfo_width(),
+                height=listframe.winfo_height())
+        rightframe.grid(row=0, column=1)
+        self.detailsbox = tk.Text(rightframe, height=21, width=40)
+        self.detailsbox.grid(row=0, column=0, sticky='nesw')
 
 
 class MainPanelUI(object):
@@ -119,6 +153,7 @@ class MainPanelUI(object):
         Plugin list is provided by the plugin manager.
         """
         available_plugins = _pad.plugin_list()
+        plp = PluginLoaderPanel(self, available_plugins)
 
     def cb_rm_plugs(self):
         pass
