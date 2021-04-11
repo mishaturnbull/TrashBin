acbab2d670707eac4a081c6c1124a52a29f6601d
Misha Turnbull <mishaturnbull@gmail.com>
2021-04-11 10:54:36 -0500

    Message remover plugin factory
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/plugins/message_remover.py
+++ src/plugins/message_remover.py
@@ -5,11 +5,14 @@
 GUI plugin for log message sanitization.
 """
 
+import os
+import tkinter as tk
+import tkinter.ttk as ttk
 import src.plugins.pluginbase as pluginbase
 import src.logutils.message_remover as message_remover
 
 
-class MessageRemoverPlugin(pluginbase.TrashBinPlugin):
+class MessageRemoverFactory(pluginbase.TBPluginFactory):
 
     author_name = "Misha Turnbull"
     author_email = "misha@turnbull.link"
@@ -22,18 +25,59 @@ class MessageRemoverPlugin(pluginbase.TrashBinPlugin):
         Create an instance of the message remover plugin for TrashBin UI.
         """
         super().__init__(handler)
-        self.filter = []
-        self.keeping = False
+        self.filter = tk.StringVar()
+        self.filter.set('.*')
+        self.whitelist = tk.BooleanVar()
+        self.whitelist.set(True)
 
-    def run_messages(self, messages):
-        pass
-        
+    @property
+    def work_per_file(self):
+        return MessageRemoverPlugin.total_work
+
     def start_ui(self, frame):
-        pass
+        self.iframe = tk.Frame(frame)
+
+        rb_white = tk.Radiobutton(self.iframe,
+                text='Keep only selected data (whitelist)',
+                variable=self.whitelist,
+                value=True,
+            )
+        rb_black = tk.Radiobutton(self.iframe,
+                text='Keep all BUT selected data (blacklist)',
+                variable=self.whitelist,
+                value=False,
+            )
+        rb_white.grid(row=0, column=0, sticky='nw')
+        rb_black.grid(row=1, column=0, sticky='nw')
+
+        filterframe = tk.Frame(frame)
+        tk.Label(filterframe, text='Message data filter').grid(
+                row=0, column=0, sticky='nw')
+        tk.Label(filterframe, text='Enter as a multi-line regex').grid(
+                row=1, column=0, sticky='nw')
+        filterbox = tk.Entry(filterframe, textvariable=self.filter)
+        filterbox.grid(row=2, column=0, sticky='nesw')
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+        filterframe.grid(row=1, column=0, sticky='nesw')
+        filterframe.grid_columnconfigure(0, weight=1)
+        filterframe.grid_rowconfigure(2, weight=1)
+        filterframe.grid_propagate(0)
+        self.filterframe = filterframe
+
+        self.iframe.grid(row=0, column=0, sticky='nesw')
+        frame.update()
 
     def stop_ui(self, frame):
-        pass
+        self.iframe.destroy()
+        self.filterframe.destroy()
+        frame.grid_rowconfigure(0, weight=0)
+        frame.grid_columnconfigure(0, weight=0)
+        frame.update()
 
     def cleanup_and_exit(self):
+        pass
+
+    def give_plugin(self):
         pass
 
